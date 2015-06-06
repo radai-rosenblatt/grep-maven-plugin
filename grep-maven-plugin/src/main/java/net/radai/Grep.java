e1bcbe12b48c0b6db90ed41e3f321983d4720987
radai-rosenblatt <radai.rosenblatt@gmail.com>
2015-06-06 22:40:29 +0300

    add support for failIfFound/failIfNotFound. implements #3

--- grep-maven-plugin/src/main/java/net/radai/Grep.java
+++ grep-maven-plugin/src/main/java/net/radai/Grep.java
@@ -5,6 +5,8 @@ public class Grep {
     private String filePattern;
     private String grepPattern;
     private String outputPattern;
+    private boolean failIfFound;
+    private boolean failIfNotFound;
 
     public String getFile() {
         return file;
@@ -38,8 +40,28 @@ public class Grep {
         this.outputPattern = outputPattern;
     }
 
+    public boolean isFailIfFound() {
+        return failIfFound;
+    }
+
+    public void setFailIfFound(boolean failIfFound) {
+        this.failIfFound = failIfFound;
+    }
+
+    public boolean isFailIfNotFound() {
+        return failIfNotFound;
+    }
+
+    public void setFailIfNotFound(boolean failIfNotFound) {
+        this.failIfNotFound = failIfNotFound;
+    }
+
     @Override
     public String toString() {
-        return "grep for \""+ grepPattern +"\" in "+ filePattern!=null ? filePattern : file;
+        String failDesc = failIfFound ? ", fail if found" : "";
+        if (failIfNotFound) {
+            failDesc += ", fail if not found";
+        }
+        return ("grep for \""+ grepPattern +"\" in "+ (filePattern!=null ? filePattern : file)) + failDesc;
     }
 }
--- grep-maven-plugin/src/main/java/net/radai/GrepMojo.java
+++ grep-maven-plugin/src/main/java/net/radai/GrepMojo.java
@@ -2,6 +2,7 @@ package net.radai;
 
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import net.java.truevfs.access.TFile;
 import net.java.truevfs.access.TFileReader;
 import org.apache.maven.plugin.AbstractMojo;
@@ -19,7 +20,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Mojo(name = "grep", threadSafe = true)
-public class GrepMojo extends AbstractMojo{
+public class GrepMojo extends AbstractMojo {
 
     @Parameter(required = true)
     private List<Grep> greps;
@@ -62,30 +63,33 @@ public class GrepMojo extends AbstractMojo{
         try {
             for (Grep grep : greps) {
                 Pattern lookingFor = Pattern.compile(grep.getGrepPattern());
-                if (grep.getFile()!=null) {
+                if (grep.getFile() != null) {
                     TFile theFile = new TFile(basedir, grep.getFile());
                     grepInFile(theFile, lookingFor, grep);
                     continue;
                 }
-                if (grep.getFilePattern()!=null) {
+                if (grep.getFilePattern() != null) {
                     log.error("file patterns not implemented yet");
                 }
             }
+        } catch (MojoFailureException e) {
+            //rethrow. this is a deliberate failure
+            throw e;
         } catch (Exception e) {
-            throw new MojoFailureException("error grepping",e);
+            throw new MojoFailureException("error grepping", e);
         }
     }
 
-    private void grepInFile(TFile theFile, Pattern lookingFor, Grep grep) throws Exception{
+    private void grepInFile(TFile theFile, Pattern lookingFor, Grep grep) throws Exception {
         if (!theFile.exists()) {
-            log.warn("specified file does not exist: "+theFile.getCanonicalPath());
+            log.warn("specified file does not exist: " + theFile.getCanonicalPath());
             return;
         }
         if (!theFile.canRead()) {
             log.warn("cannot read from file " + theFile.getCanonicalPath());
             return;
         }
-        log.info("grepping for "+lookingFor+" in "+theFile.getCanonicalPath());
+        log.info("grepping for " + lookingFor + " in " + theFile.getCanonicalPath());
         Matcher m;
         TFileReader reader = null;
         try {
@@ -93,35 +97,61 @@ public class GrepMojo extends AbstractMojo{
             BufferedReader bufferedReader = new BufferedReader(reader);
             String line;
             int lineNumber = 0;
-            while ((line=bufferedReader.readLine())!=null) {
+            boolean found = false;
+            while ((line = bufferedReader.readLine()) != null) {
                 lineNumber++;
                 m = lookingFor.matcher(line);
                 if (!(m.find())) {
                     continue;
                 }
                 processMatchingLine(theFile, line, grep, lineNumber);
+                found = true;
+            }
+            if (!found) {
+                failIfNotFound(theFile, grep);
             }
         } finally {
-            if (reader!=null) reader.close();
+            if (reader != null) reader.close();
         }
     }
 
     private void processMatchingLine(TFile theFile, String theLine, Grep grep, int lineNumber) throws Exception {
+        printMatch(theFile, theLine, grep, lineNumber);
+        failIfFound(theFile, theLine, grep, lineNumber);
+    }
+
+    private void printMatch(TFile theFile, String theLine, Grep grep, int lineNumber) throws IOException, TemplateException {
         String templateToUse = grep.getOutputPattern();
-        if (templateToUse==null) {
+        if (templateToUse == null) {
             templateToUse = outputPattern;
         }
-        if (templateToUse==null) {
+        if (templateToUse == null) {
             log.info(theLine);
-            return;
+        } else {
+            Template template = new Template("templateName", new StringReader(templateToUse), new Configuration());
+            HashMap<String, String> parameters = new HashMap<>();
+            parameters.put("line", theLine);
+            parameters.put("fileName", theFile.getName());
+            parameters.put("lineNumber", lineNumber + "");
+            StringWriter output = new StringWriter();
+            template.process(parameters, output);
+            log.info(output.toString());
         }
-        Template template = new Template("templateName", new StringReader(templateToUse), new Configuration());
-        HashMap<String,String> parameters = new HashMap<String, String>();
-        parameters.put("line",theLine);
-        parameters.put("fileName",theFile.getName());
-        parameters.put("lineNumber",lineNumber+"");
-        StringWriter output = new StringWriter();
-        template.process(parameters, output);
-        log.info(output.toString());
+    }
+
+    private void failIfFound(TFile theFile, String theLine, Grep grep, int lineNumber) throws IOException, MojoFailureException {
+        if (grep.isFailIfFound()) {
+            String msg = grep.getGrepPattern() + " found in  " + theFile.getCanonicalPath() + ":" + lineNumber + " (" + theLine + ")";
+            log.error(msg);
+            throw new MojoFailureException(msg);
+        }
+    }
+
+    private void failIfNotFound(TFile theFile, Grep grep) throws IOException, MojoFailureException {
+        if (grep.isFailIfNotFound()) {
+            String msg = grep.getGrepPattern() + " not found in  " + theFile.getCanonicalPath();
+            log.error(msg);
+            throw new MojoFailureException(msg);
+        }
     }
 }
